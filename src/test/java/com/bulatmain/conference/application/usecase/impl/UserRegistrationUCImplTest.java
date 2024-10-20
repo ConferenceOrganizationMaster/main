2fa76f8020a050c09fa2542999ae31bfeb09b1f2
bulat <sinntexxx@gmail.com>
2024-10-20 15:41:59 +0300

    added UserRegistrationUCImplTest

---
+++ src/test/java/com/bulatmain/conference/application/usecase/impl/UserRegistrationUCImplTest.java
@@ -0,0 +1,113 @@
+package com.bulatmain.conference.application.usecase.impl;
+
+import com.bulatmain.conference.application.model.UserDTO;
+import com.bulatmain.conference.application.model.UserRegistrationRequestData;
+import com.bulatmain.conference.application.port.UserGateway;
+import com.bulatmain.conference.application.usecase.impl.exception.UserAlreadyExistsException;
+import com.bulatmain.conference.domain.common.value.validator.Validator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+
+@ExtendWith(MockitoExtension.class)
+public class UserRegistrationUCImplTest {
+    @Mock
+    private UserGateway userGateway;
+    @Mock
+    private Validator<String> emailValidator;
+    @Mock
+    private Validator<String> loginValidator;
+    @Mock
+    private Validator<String> passwordValidator;
+
+    private UserRegistrationUCImpl userRegistrationUC;
+
+    @BeforeEach
+    void initUC() {
+        userRegistrationUC = new UserRegistrationUCImpl(
+                userGateway,
+                emailValidator,
+                loginValidator,
+                passwordValidator
+        );
+    }
+
+    @Test
+    public void RegisterNewValidUserEmptyRepo_NoThrow() {
+        var requestData = requestDataExample1();
+
+        mockGatewayRegister();
+        mockValidators(
+                true,
+                true,
+                true
+        );
+
+        Assertions.assertDoesNotThrow(
+                () -> userRegistrationUC.execute(requestData));
+    }
+
+    @Test
+    public void RegisterUserWithExistingEmail_ThrowsException() {
+        var requestData = requestDataExample1();
+
+        mockGatewayFindUserByEmail(requestData.toDTO());
+        mockGatewayRegister();
+        mockValidators(
+                true,
+                true,
+                true
+        );
+
+        Assertions.assertThrows(
+                UserAlreadyExistsException.class,
+                () -> userRegistrationUC.execute(requestData)
+        );
+    }
+
+    private UserRegistrationRequestData requestDataExample1() {
+        return  UserRegistrationRequestData.builder()
+                .email("exampe@gmail.com")
+                .login("example.login")
+                .password("example.password123#")
+                .build();
+    }
+
+    private void mockGatewayFindUserByEmail(UserDTO userDTO) {
+        Mockito.doReturn(Optional.ofNullable(userDTO))
+                .when(userGateway)
+                .findUserByEmail(Mockito.anyString());
+    }
+
+    private void mockGatewayRegister() {
+        Mockito.lenient()
+                .doNothing()
+                .when(userGateway)
+                .register(Mockito.any(UserDTO.class));
+    }
+
+    private void mockValidators(
+            boolean forEmail,
+            boolean forLogin,
+            boolean forPassword
+    ) {
+        mockValidator(emailValidator, forEmail);
+        mockValidator(loginValidator, forLogin);
+        mockValidator(passwordValidator, forPassword);
+    }
+
+    private void mockValidator(Validator<String> validator, boolean returned) {
+        Mockito.lenient()
+                .doReturn(returned)
+                .when(validator)
+                .check(Mockito.anyString());
+    }
+}
--- src/test/java/com/bulatmain/conference/infrastructure/controller/UserControllerTest.java
+++ src/test/java/com/bulatmain/conference/infrastructure/controller/UserControllerTest.java
@@ -1,6 +1,17 @@
 package com.bulatmain.conference.infrastructure.controller;
 
-public class UserControllerTest {
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.TestPropertySource;
 
+@WebMvcTest(controllers = UserController.class)
+@AutoConfigureMockMvc(addFilters = false)
+@ExtendWith(MockitoExtension.class)
+@TestPropertySource(locations = "classpath:test.properties")
+public class UserControllerTest {
+    // TODO: add tests for implemented methods
 
 }
